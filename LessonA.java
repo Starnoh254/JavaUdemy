b2adab74787a3b024dc60677b38debab4c3147af
Starnoh John <130779431+Starnoh254@users.noreply.github.com>
2026-01-25 18:07:18 +0300

    51. Anonymous Object

---
+++ LessonA.java
@@ -0,0 +1,27 @@
+public class LessonA {
+
+    public static void main(String[] args) {
+        // anonymous object
+        new Student("Starnoh John", "BSSEC01/1756/2022", 22).printDetails();
+    }
+}
+
+
+class Student {
+
+    String name;
+    String admission;
+    int age;
+
+
+    Student (String name , String admission , int age) {
+        this.name = name;
+        this.admission = admission;
+        this.age = age;
+
+    }
+
+    public void printDetails(){
+        System.out.println("Name : " + this.name + " Admission No : " + this.admission + " of age : " + this.age);
+    }
+}
