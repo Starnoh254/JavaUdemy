43d8e25b7ac785fcce253152c588aa385ec66159
Starnoh John <130779431+Starnoh254@users.noreply.github.com>
2026-02-13 18:17:21 +0300

    18. if Else if

--- lessons/LessonN.java
+++ lessons/LessonN.java
@@ -4,23 +4,17 @@ public class LessonN {
     public static void main(String[] args){
         int x = 800;
         int y = 7;
-        int z = 46;
+        int z = 4600;
 
-        if(x > y){
-            if(x > z){
-                System.out.println(x);
-            }
-            else{
-                System.out.println(z);
-            }
+        if(x > y && x > z){
+            System.out.println(x);
         }
-        else {
-            if(y > z){
-                System.out.println(y);
-            }
-            else{
-                System.out.println(z);
-            }
+        else if (y > x && y > z){
+            System.out.println(y);
+        }else {
+            System.out.println(z);
         }
+
+
     }
 }
