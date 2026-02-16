eeea1f7c0cace78891940bc82710d4327e271b42
Starnoh John <130779431+Starnoh254@users.noreply.github.com>
2026-02-16 11:19:49 +0300

    19. Ternary

--- lessons/LessonN.java
+++ lessons/LessonN.java
@@ -9,7 +9,7 @@ public class LessonN {
         if(x > y && x > z){
             System.out.println(x);
         }
-        else if (y > x && y > z){
+        else if (y > z){
             System.out.println(y);
         }else {
             System.out.println(z);
---
+++ lessons/LessonO.java
@@ -0,0 +1,17 @@
+package lessons;
+
+public class LessonO {
+    public static void main(String[] args){
+
+        // int age = 2;
+
+        // String result = (age >= 18) ? "Adult" : "Minor";
+
+        
+        int x = -0;
+        
+        String result = (x > 0) ? "Positive" : (x < 0) ? "Negative" : "Zero";
+        
+        System.out.println(result);
+    }
+}
