f84adf11595d0e5461b6bc0a99990c3d5f642992
Starnoh John <130779431+Starnoh254@users.noreply.github.com>
2026-02-17 12:30:36 +0300

    23. Do While Loop

---
+++ lessons/LessonR.java
@@ -0,0 +1,12 @@
+package lessons;
+
+public class LessonR {
+    public static void main(String[] args){
+        int i = 5;
+
+        do {
+            System.out.println("Hi " + i);
+            i++;
+        } while(i < 5);
+    }
+}
