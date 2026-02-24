2d352efb61120e271b46367889d998c969eef0cc
Starnoh John <130779431+Starnoh254@users.noreply.github.com>
2026-02-24 15:22:36 +0300

    88. try with multiple catch

--- lessons/LessonY.java
+++ lessons/LessonY.java
@@ -3,11 +3,20 @@ package lessons;
 public class LessonY {
     public static void main(String[] args) {
 
+        int nums[] = new int[5];
+        String str = null;
+
         try {
-            int s = 10 / 0;
+            int s = 10 / 2;
+            System.out.println(str.length());
             System.out.println(s);
+            System.out.println(nums[5]);
+        } catch (ArithmeticException e) {
+            System.out.println("Cannot divide by zero");
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Stay in your limit");
         } catch (Exception e) {
-            System.out.println("An error occurred : " + e.getMessage());
+            System.out.println("Something went wrong : " + e);
         }
 
         System.out.println("Bye");
