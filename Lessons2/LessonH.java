e80e720fd765b899cb840639d74c87541a8efd02
Starnoh John <130779431+Starnoh254@users.noreply.github.com>
2026-03-04 14:26:06 +0300

    91 . Custom Exception Improvement

--- Lessons2/LessonH.java
+++ Lessons2/LessonH.java
@@ -1,21 +1,25 @@
 package Lessons2;
 
-class InvalidNameException extends Exception {
-    public InvalidNameException(String message){
+class InvalidNameException extends RuntimeException {
+    public InvalidNameException(String message) {
         super(message);
     }
 }
 
 public class LessonH {
-    public static void main(String[] args) throws InvalidNameException{
+    public static void main(String[] args) {
 
-        String name = "Starnoh";
+        try {
+            String name = "Starnggoh";
 
-        if(!name.equals("Stan")){
-            throw new InvalidNameException("The name is not Starnoh");
+            if (!name.equals("Starnoh")) {
+                throw new InvalidNameException("The name is not Starnoh");
+            }
+
+            System.out.println("The name is definitley Starnoh");
+        } catch (InvalidNameException e) {
+            System.out.println("Caught Exception: " + e.getMessage());
         }
 
-        System.out.println("The name is definitley Starnoh");
-        
     }
 }
